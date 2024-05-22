0ce4f9d8269cf1119255f95f32a641cc2b2a0a38
Matteo Gambella <55230920+matteogambella@users.noreply.github.com>
2024-05-22 10:12:24 +0200

    first commit

---
+++ architect.py
@@ -0,0 +1,199 @@
+import numpy as np
+import torch
+from torch.autograd import Variable, grad
+import sys
+import torch.nn.functional as F
+
+
+def _concat(xs):
+    return torch.cat([x.view(-1) for x in xs])
+
+
+class Architect(object):
+
+    def __init__(self, model, args):
+        self.network_momentum = args.momentum
+        self.network_weight_decay = args.weight_decay
+        self.model = model
+        self.optimizer = torch.optim.Adam(self.model.arch_parameters(),
+                                          lr=args.arch_learning_rate, betas=(0.5, 0.999),
+                                          weight_decay=args.arch_weight_decay)
+
+        self.rho_alpha=1e-2
+        self.epsilon=1e-2
+
+    def _train_loss(self, model, input, target):
+        return model._loss(input, target)
+
+    def _val_loss(self, model, input, target):
+        return model._loss(input, target)
+    
+    def zero_hot(self, norm_weights):
+        # pos = (norm_weights == norm_weights.max(axis=1, keepdims=1))
+        valid_loss = torch.log(norm_weights)
+        base_entropy = torch.log(torch.tensor(2).float())
+        aux_loss = torch.mean(valid_loss) + base_entropy
+        return aux_loss
+
+    def mlc_loss(self, arch_param):
+        y_pred_neg = arch_param
+        neg_loss = torch.logsumexp(y_pred_neg, dim=-1)
+        aux_loss = torch.mean(neg_loss)
+        return aux_loss
+
+    def mlc_pos_loss(self, arch_param):
+        act_param = F.softmax(arch_param, dim=-1)
+        # thr = act_param.min(axis=-1, keepdim=True)[0]*1.2  # try other methods
+        thr = act_param.max(axis=-1, keepdim=True)[0]
+        y_true = (act_param >= thr)
+        arch_param_new = (1 - 2 * y_true) * arch_param
+        y_pred_neg = arch_param_new - y_true * 1e12
+        y_pred_pos = arch_param_new - ~y_true * 1e12
+        neg_loss = torch.logsumexp(y_pred_neg, dim=-1)
+        pos_loss = torch.logsumexp(y_pred_pos, dim=-1)
+        aux_loss = torch.mean(neg_loss)+torch.mean(pos_loss)
+        return aux_loss
+
+    def mlc_loss2(self, arch_param):
+        y_pred_neg = arch_param
+        neg_loss = torch.log(torch.exp(y_pred_neg))
+        aux_loss = torch.mean(neg_loss)
+        return aux_loss
+
+    def _compute_unrolled_model(self, input, target, eta, network_optimizer):
+        loss = self._train_loss(model=self.model, input=input, target=target)
+        theta = _concat(self.model.parameters()).data
+        try:
+            moment = _concat(network_optimizer.state[v]['momentum_buffer'] for v in self.model.parameters()).mul_(
+                self.network_momentum)
+        except:
+            moment = torch.zeros_like(theta)
+        dtheta = _concat(torch.autograd.grad(loss, self.model.parameters())).data + self.network_weight_decay * theta
+        unrolled_model = self._construct_model_from_theta(theta.sub(moment + dtheta,alpha=eta))
+        return unrolled_model
+
+    def step(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer, epoch, unrolled, betadecay, sam):
+        self.optimizer.zero_grad()
+        if sam:
+            self._backward_step_SAM(input_valid, target_valid)
+        else:
+            if unrolled: 
+                self._backward_step_unrolled(input_train, target_train, input_valid, target_valid, eta, network_optimizer)
+            else:
+                self._backward_step(input_valid, target_valid, epoch, betadecay)
+        self.optimizer.step()
+
+    def _backward_step(self, input_valid, target_valid, epoch, betadecay):
+
+        if betadecay: #Beta-DARTS
+            weights = 0 + 50*epoch/100
+            ssr_normal = self.mlc_loss(self.model._arch_parameters)
+            loss = self.model._loss(input_valid, target_valid) + weights*ssr_normal
+        else: #original DARTS
+            loss = self.model._loss(input_valid, target_valid)        
+        loss.backward()
+
+    def _backward_step_unrolled(self, input_train, target_train, input_valid, target_valid, eta, network_optimizer):
+        unrolled_model = self._compute_unrolled_model(input_train, target_train, eta, network_optimizer)
+        unrolled_loss = self._val_loss(model=unrolled_model, input=input_valid, target=target_valid)
+
+        # Compute backwards pass with respect to the unrolled model parameters
+        unrolled_loss.backward()
+        dalpha = [v.grad for v in unrolled_model.arch_parameters()]
+        vector = [v.grad.data for v in unrolled_model.parameters()]
+
+        # Compute expression (8) from paper
+        implicit_grads = self._hessian_vector_product(vector, input_train, target_train)
+
+        # Compute expression (7) from paper
+        for g, ig in zip(dalpha, implicit_grads):
+            g.data.sub_(eta, ig.data)
+
+        for v, g in zip(self.model.arch_parameters(), dalpha):
+            if v.grad is None:
+                v.grad = Variable(g.data)
+            else:
+                v.grad.data.copy_(g.data)
+
+    def _construct_model_from_theta(self, theta):
+        model_new = self.model.new()
+        model_dict = self.model.state_dict()
+
+        params, offset = {}, 0
+        for k, v in self.model.named_parameters():
+            v_length = np.prod(v.size())
+            params[k] = theta[offset: offset + v_length].view(v.size())
+            offset += v_length
+
+        assert offset == len(theta)
+        model_dict.update(params)
+        model_new.load_state_dict(model_dict)
+        return model_new.cuda()
+    
+    def _hessian_vector_product(self, vector, input, target, r=1e-2):
+        R = r / _concat(vector).norm()
+        for p, v in zip(self.model.parameters(), vector):
+            p.data.add_(R, v)
+        loss = self._train_loss(self.model, input=input, target=target)
+        grads_p = torch.autograd.grad(loss, self.model.arch_parameters())
+
+        for p, v in zip(self.model.parameters(), vector):
+            p.data.sub_(2 * R, v)
+        loss = self._train_loss(self.model, input=input, target=target)
+        grads_n = torch.autograd.grad(loss, self.model.arch_parameters())
+
+        for p, v in zip(self.model.parameters(), vector):
+            p.data.add_(R, v)
+
+        return [(x - y).div_(2 * R) for x, y in zip(grads_p, grads_n)]
+    
+    def _backward_step_SAM(self, input_val, target_val):
+        """Calculates the first-order approximation of the architecture gradient."""
+        # Compute \tilde{\alpha}_{\xi=0}
+        val_loss = self._val_loss(self.model, input_val, target_val)
+        dL_val_dalpha = torch.autograd.grad(val_loss, self.model.arch_parameters())
+
+        tilde_alpha = [alpha + self.rho_alpha * dalpha for alpha, dalpha in zip(self.model.arch_parameters(), dL_val_dalpha)]
+
+        # Save the current state of the architecture parameters
+        current_alpha = [alpha.clone() for alpha in self.model.arch_parameters()]
+
+        # Update model parameters to alpha_tilde and compute val_loss_tilde
+        with torch.no_grad():
+            for param, alpha_p in zip(self.model.arch_parameters(), tilde_alpha):
+                param.data.copy_(alpha_p)
+        val_loss_tilde = self._val_loss(self.model, input_val, target_val)
+        dL_val_dtilde_alpha = torch.autograd.grad(val_loss_tilde, self.model.arch_parameters())
+
+        alpha_plus = [alpha + self.epsilon * dalpha for alpha, dalpha in zip(self.model.arch_parameters(), dL_val_dtilde_alpha)]
+        alpha_minus = [alpha - self.epsilon * dalpha for alpha, dalpha in zip(self.model.arch_parameters(), dL_val_dtilde_alpha)]
+
+        # Update model parameters to alpha_plus and compute val_loss_plus
+        with torch.no_grad():
+            for param, alpha_p in zip(self.model.arch_parameters(), alpha_plus):
+                param.data.copy_(alpha_p)
+        val_loss_plus = self._val_loss(self.model, input_val, target_val)
+        dL_val_plus = torch.autograd.grad(val_loss_plus, self.model.arch_parameters())
+
+        # Update model parameters to alpha_minus and compute val_loss_minus
+        with torch.no_grad():
+            for param, alpha_m in zip(self.model.arch_parameters(), alpha_minus):
+                param.data.copy_(alpha_m)
+        val_loss_minus = self._val_loss(self.model, input_val, target_val)
+        dL_val_minus = torch.autograd.grad(val_loss_minus, self.model.arch_parameters())
+
+        # Restore the original architecture parameters
+        with torch.no_grad():
+            for param, alpha_c in zip(self.model.arch_parameters(), current_alpha):
+                param.data.copy_(alpha_c)
+
+        finite_diff = [(plus - minus).div_(2 * self.epsilon) for plus, minus in zip(dL_val_plus, dL_val_minus)]
+
+        first_order_approx = [dL_val_dalpha + self.rho_alpha * fd for dL_val_dalpha, fd in zip(dL_val_dtilde_alpha, finite_diff)]
+
+        for v, g in zip(self.model.arch_parameters(), first_order_approx):
+            if v.grad is None:
+                v.grad = Variable(g.data)
+            else:
+                v.grad.data.copy_(g.data)
+
---
+++ train_search.py
@@ -0,0 +1,278 @@
+import os
+import sys
+import time
+import glob
+import numpy as np
+import torch
+import utils
+import logging
+import argparse
+import torch.nn as nn
+import torch.utils
+import torch.nn.functional as F
+import torchvision.datasets as dset
+import torch.backends.cudnn as cudnn
+
+from torch.autograd import Variable
+from darts.model_search import Network
+from nasbench201.model_search import BenchNetwork
+from architect import Architect
+from utils import get_data_loaders
+from genotypes import BENCH_PRIMITIVES
+
+def translate_genotype_to_encode(genotype):
+    dag_integers = []
+    for node_op, _ in genotype.normal:
+        dag_integers.append(BENCH_PRIMITIVES.index(node_op))
+    return dag_integers
+
+def write_array_to_file(arr, file_path):
+    with open(file_path, 'w') as file:
+        array_str = ','.join(map(str, arr))  # Join integers into a string separated by comma
+        file.write(array_str)
+
+parser = argparse.ArgumentParser("cifar")
+parser.add_argument('--data', type=str, default='../../datasets', help='location of the data corpus')
+parser.add_argument('--dataset', type=str, default='cifar10', help='choose dataset')
+parser.add_argument('--workers', type=int, default=4, help='number of data loading workers')
+parser.add_argument('--save', type=str, default='../../results/darts', help='location of the exp')
+parser.add_argument('--nasbench', action='store_true', default=False, help='use nasbench search space')
+parser.add_argument('--batch_size', type=int, default=64, help='batch size')
+parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
+parser.add_argument('--learning_rate_min', type=float, default=0.001, help='min learning rate')
+parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
+parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
+parser.add_argument('--nesterov', action='store_true', default=False, help='use nesterov momentum')
+parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
+parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+parser.add_argument('--epochs', type=int, default=50, help='num of training epochs')
+parser.add_argument('--init_channels', type=int, default=16, help='num of init channels')
+parser.add_argument('--layers', type=int, default=8, help='total number of layers') 
+parser.add_argument('--model_path', type=str, default='saved_models', help='path to save the model')
+parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+parser.add_argument('--cutout_prob', type=float, default=1.0, help='cutout probability')
+parser.add_argument('--drop_path_prob', type=float, default=0.3, help='drop path probability')
+parser.add_argument('--seed', type=int, default=2, help='random seed')
+parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
+parser.add_argument('--train_portion', type=float, default=0.5, help='portion of training data')
+parser.add_argument('--unrolled', action='store_true', default=False, help='use one-step unrolled validation loss')
+parser.add_argument('--betadecay', action='store_true', default=False, help='use beta-darts regularization')
+parser.add_argument('--sam', action='store_true', default=False, help='use sam update rule')
+parser.add_argument('--arch_learning_rate', type=float, default=3e-4, help='learning rate for arch encoding')
+parser.add_argument('--arch_weight_decay', type=float, default=1e-3, help='weight decay for arch encoding')
+args = parser.parse_args()
+
+utils.create_exp_dir(args.save, scripts_to_save=glob.glob('*.py'))
+
+log_format = '%(asctime)s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+fh = logging.FileHandler(os.path.join(args.save, 'log.txt'))
+fh.setFormatter(logging.Formatter(log_format))
+logging.getLogger().addHandler(fh)
+
+#CIFAR_CLASSES = 10
+if args.dataset == 'cifar100':
+    n_classes = 100
+else:
+    n_classes = 10
+
+def main():
+  if not torch.cuda.is_available():
+    logging.info('no gpu device available')
+    sys.exit(1)
+
+  np.random.seed(args.seed)
+  torch.cuda.set_device(args.gpu)
+  cudnn.benchmark = True
+  torch.manual_seed(args.seed)
+  cudnn.enabled=True
+  torch.cuda.manual_seed(args.seed)
+  logging.info('gpu device = %d' % args.gpu)
+  logging.info("args = %s", args)
+
+  criterion = nn.CrossEntropyLoss()
+  criterion = criterion.cuda()
+
+  if not args.nasbench:
+    model = Network(args.init_channels, n_classes, args.layers, criterion)
+  else:
+    stages = 3
+    cells = 5
+    model = BenchNetwork(args.init_channels, n_classes, stages, cells, criterion)
+
+  model = model.cuda()
+  logging.info("param size = %fMB", utils.count_parameters_in_MB(model))
+
+  optimizer = torch.optim.SGD(
+      model.parameters(),
+      args.learning_rate,
+      momentum=args.momentum,
+      weight_decay=args.weight_decay,
+      nesterov=args.nesterov)
+  
+  res=32
+  
+  train_queue, valid_queue, test_queue = get_data_loaders(dataset='cifar10', batch_size=args.batch_size, threads=args.workers, 
+                                            val_split=args.train_portion, img_size=res, augmentation=True, eval_test=True)
+
+  scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, float(args.epochs), eta_min=args.learning_rate_min)
+  
+  # Check if CUDA (GPU support) is available
+  if torch.cuda.is_available():
+      device = torch.device("cuda")  # Use GPU
+      print("CUDA is available! Using GPU.")
+  else:
+      device = torch.device("cpu")   # Use CPU
+      print("CUDA is not available. Using CPU.")
+
+  if args.sam:
+    logging.info('Using new SAM update rule')
+  else:
+    if args.betadecay:
+        logging.info('Using Beta-DARTS')
+    else:
+        logging.info('Using original DARTS')
+
+  architect = Architect(model, args)
+
+  best_loss= float('inf')
+  best_epoch=0
+  best_acc=0
+  best_genotype=None
+
+  for epoch in range(args.epochs):
+    #lr = scheduler.get_lr()[0]
+    lr = scheduler.get_last_lr()[0]
+    logging.info('epoch %d lr %e', epoch, lr)
+
+    if args.cutout:
+        # increase the cutout probability linearly throughout search
+        train_transform.transforms[-1].cutout_prob = args.cutout_prob * epoch / (args.epochs - 1)
+        logging.info('epoch %d lr %e cutout_prob %e', epoch, lr,
+                      train_transform.transforms[-1].cutout_prob)
+    else:
+        logging.info('epoch %d lr %e', epoch, lr)
+
+    genotype = model.genotype()
+    logging.info('genotype = %s', genotype)
+
+    print(model.show_alphas())
+
+    # training
+    train_acc, train_obj = train(train_queue, valid_queue, model, architect, criterion, optimizer, lr, epoch)
+    logging.info('train_acc %f, train_loss %f', train_acc, train_obj)
+
+    # validation
+    valid_acc, valid_obj = infer(valid_queue, model, criterion)
+    logging.info('valid_acc %f, val_loss %f', valid_acc, valid_obj)
+    scheduler.step()
+
+    if valid_obj < best_loss:
+      logging.info('Best model found at epoch %d', epoch)
+      best_loss = valid_obj
+      best_epoch = epoch
+      best_acc = valid_acc
+      best_genotype = genotype
+      utils.save(model, os.path.join(args.save, 'weights.pt'))
+  
+  # Info about best searched model
+
+  logging.info('Best model found at epoch %d', best_epoch) 
+  logging.info('Best genotype: %s', best_genotype)
+  logging.info('Best validation loss: %f', best_loss)
+  logging.info('Best validation accuracy: %f', best_acc)
+  cell_encode = translate_genotype_to_encode(best_genotype)
+  write_array_to_file(cell_encode, os.path.join(args.save, 'best_genotype.txt'))
+
+
+def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr, epoch): #perturb_alpha, epsilon_alpha
+    objs = utils.AvgrageMeter()
+    top1 = utils.AvgrageMeter()
+    top5 = utils.AvgrageMeter()
+
+    for step, (input, target) in enumerate(train_queue):
+        model.train()
+        n = input.size(0)
+
+        input = input.cuda()
+        target = target.cuda(non_blocking=True)
+
+        # get a random minibatch from the search queue with replacement
+        input_search, target_search = next(iter(valid_queue))
+        input_search = input_search.cuda()
+        target_search = target_search.cuda(non_blocking=True)
+
+        architect.step(input, target, input_search, target_search, lr, optimizer, epoch, unrolled=args.unrolled, betadecay=args.betadecay,
+                       sam=args.sam)
+        optimizer.zero_grad()
+        architect.optimizer.zero_grad()
+
+        # print('before softmax', model.arch_parameters())
+        model.softmax_arch_parameters()
+
+        '''
+        # perturb on alpha
+        # print('after softmax', model.arch_parameters())
+        if perturb_alpha:
+            perturb_alpha(model, input, target, epsilon_alpha)
+            optimizer.zero_grad()
+            architect.optimizer.zero_grad()
+        # print('after perturb', model.arch_parameters())
+        '''
+
+        logits = model(input) #, updateType='weight')
+        loss = criterion(logits, target)
+
+        loss.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), args.grad_clip)
+        optimizer.step()
+        model.restore_arch_parameters()
+        # print('after restore', model.arch_parameters())
+
+        prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+        objs.update(loss.data, n)
+        top1.update(prec1.data, n)
+        top5.update(prec5.data, n)
+
+        if step % args.report_freq == 0:
+            logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+            if 'debug' in args.save:
+                break
+
+    return  top1.avg, objs.avg
+
+
+def infer(valid_queue, model, criterion):
+    objs = utils.AvgrageMeter()
+    top1 = utils.AvgrageMeter()
+    top5 = utils.AvgrageMeter()
+    model.eval()
+
+    with torch.no_grad():
+        for step, (input, target) in enumerate(valid_queue):
+            input = input.cuda()
+            target = target.cuda(non_blocking=True)
+
+            logits = model(input)
+            loss = criterion(logits, target)
+
+            prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+            n = input.size(0)
+            objs.update(loss.data, n)
+            top1.update(prec1.data, n)
+            top5.update(prec5.data, n)
+
+            if step % args.report_freq == 0:
+                logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+                if 'debug' in args.save:
+                    break
+
+    return top1.avg, objs.avg
+
+
+if __name__ == '__main__':
+  main() 
+
