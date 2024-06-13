d57c9166077cc76161be0e17a863a2735f3b732d
Matteo Gambella <55230920+matteogambella@users.noreply.github.com>
2024-06-13 16:38:31 +0200

    remove stuff

--- scripts/darts1st-nasbench-search.sh
+++ scripts/darts1st-nasbench-search.sh
@@ -1,7 +1,11 @@
 #1st order approx: not unrolled 
 
-python nasbench201/train_search.py --nasbench --save results/nasbench_search_1storder_seed41val --dataset cifar10 \
+python nasbench201/train_search.py --nasbench --save results/nasbench_search_1storder --dataset cifar10 \
                        --batch_size 64 --weight_decay 3e-4 --epochs 50 --momentum 0.9 \
                        --learning_rate 0.025 --learning_rate_min 0.001 \
                        --init_channels 16 --grad_clip 5 \
+<<<<<<< HEAD
                        --arch_learning_rate 3e-4 --arch_weight_decay 1e-3 --data_aug --wandb
+=======
+                       --arch_learning_rate 3e-4 --arch_weight_decay 1e-3 --wandb
+>>>>>>> parent of 97b7cb5 (add data_aug flag)
