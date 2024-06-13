f836ecd24da3a827c507e75f5319c61978ff6dc4
matteogambella <matteogambella@gmail.com>
2024-06-13 14:40:20 +0000

    update

--- scripts/beta-nasbench-search.sh
+++ scripts/beta-nasbench-search.sh
@@ -7,4 +7,4 @@ python nasbench201/train_search.py --nasbench --save results/nasbench_search_bet
                        --batch_size 64 --weight_decay 3e-4 --epochs 50 --momentum 0.9 \
                        --learning_rate 0.025 --learning_rate_min 0.001 --init_channels 16 --grad_clip 5 \
                        --arch_learning_rate 3e-4 --arch_weight_decay 1e-3 \
-                       --beta --flood_level $flood_level --wandb
+                       --beta --flood_level $flood_level --data_aug --wandb
