396c5eb3dbb9756616a64fffb3283df49a73fc87
fabriziopittorino <fabrizio.pittorino@gmail.com>
2024-06-20 17:29:18 +0200

    update script

--- scripts/darts-nasbench-search.sh
+++ scripts/darts-nasbench-search.sh
@@ -1,19 +1,27 @@
 #Beta-DARTS with flood weight regularization
-dataset=imagenet16
+# imagenet16, cifar10, cifar100
+dataset=cifar100
 flood_level=0.0
 betadecay=False
 unrolled=False
 data_aug=False
 wandb=True
 sam=True
-rho_alpha_sam=1e-3
-epsilon_sam=1e-2
+rho_alpha_sams=(1e-5 1e-4 1e-3 1e-2 1e-1 1.0 2.0)
+epsilon_sams=(1e-2)
 
-python nasbench201/train_search.py --nasbench --save results/nasbench_search_dataset${dataset}_betadecay${betadecay}_unrolled${unrolled}_data_aug${data_aug} \
-                       --dataset $dataset --data ../datasets/$dataset \
-                       --batch_size 64 --weight_decay 3e-4 --epochs 50 --momentum 0.9 \
-                       --learning_rate 0.025 --learning_rate_min 0.001 --init_channels 16 --grad_clip 5 \
-                       --arch_learning_rate 3e-4 --arch_weight_decay 1e-3 \
-                       --betadecay $betadecay --flood_level $flood_level \
-                       --data_aug $data_aug --unrolled $unrolled --wandb $wandb \
-                       --sam $sam --epsilon_sam $epsilon_sam --rho_alpha_sam $rho_alpha_sam
+for rho_alpha_sam in "${rho_alpha_sams[@]}"; do
+    for epsilon_sam in "${epsilon_sams[@]}"; do 
+        python nasbench201/train_search.py --nasbench --save results/nasbench_search_dataset${dataset}_betadecay${betadecay}_unrolled${unrolled}_data_aug${data_aug} \
+                        --dataset $dataset --data ../datasets/$dataset \
+                        --batch_size 64 --weight_decay 3e-4 --epochs 50 --momentum 0.9 \
+                        --learning_rate 0.025 --learning_rate_min 0.001 --init_channels 16 --grad_clip 5 \
+                        --arch_learning_rate 3e-4 --arch_weight_decay 1e-3 \
+                        --betadecay $betadecay --flood_level $flood_level \
+                        --data_aug $data_aug --unrolled $unrolled --wandb $wandb \
+                        --sam $sam --epsilon_sam $epsilon_sam --rho_alpha_sam $rho_alpha_sam
+        echo "One of the processes has finished. Continuing..."
+    done
+done
+
+echo "All experiments have been launched."
