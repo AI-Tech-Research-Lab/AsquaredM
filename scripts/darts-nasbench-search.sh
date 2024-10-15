c9a31bc2fea46a70053775c63e1179bc99c91ef7
fabriziopittorino <fabrizio.pittorino@gmail.com>
2024-10-15 18:14:04 +0200

    merge

--- scripts/darts-nasbench-search.sh
+++ scripts/darts-nasbench-search.sh
@@ -1,19 +1,18 @@
 #Beta-DARTS with flood weight regularization
 # imagenet16, cifar10, cifar100
-dataset=cifar100
+dataset=cifar10
 sam=True
-betadecay=True
+betadecay=False
 unrolled=False
 data_augs=(False True)
-rho_alpha_sams=(1e-5 1e-4 1e-3 1e-2 1e-1 1.0 2.0)
-
+data_aug=True
+rho_alpha_sams=(1e-4 1e-2 1e-1)
 epsilon_sam=1e-2
 flood_level=0.0
 wandb=True
 
 for rho_alpha_sam in "${rho_alpha_sams[@]}"; do
-    for data_aug in "${data_augs[@]}"; do 
-        python nasbench201/train_search.py --nasbench --save results/nasbench_search_dataset${dataset}_betadecay${betadecay}_unrolled${unrolled}_data_aug${data_aug} \
+        python train_search.py --nasbench --save results/nasbench_search_dataset${dataset}_betadecay${betadecay}_unrolled${unrolled}_data_aug${data_aug} \
                         --dataset $dataset --data ../datasets/$dataset \
                         --batch_size 64 --weight_decay 3e-4 --epochs 50 --momentum 0.9 \
                         --learning_rate 0.025 --learning_rate_min 0.001 --init_channels 16 --grad_clip 5 \
@@ -22,7 +21,7 @@ for rho_alpha_sam in "${rho_alpha_sams[@]}"; do
                         --data_aug $data_aug --unrolled $unrolled --wandb $wandb \
                         --sam $sam --epsilon_sam $epsilon_sam --rho_alpha_sam $rho_alpha_sam
         echo "One of the processes has finished. Continuing..."
-    done
 done
 
 echo "All experiments have been launched."
+#for data_aug in "${data_augs[@]}"; do 
