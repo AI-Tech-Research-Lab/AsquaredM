04fc0301273fdbf7747d75e42ad5d288bb9e7a13
Pittorino <fabrizio.pittorino@gmail.com>
2024-11-13 15:52:42 +0100

    add bad arch

--- scripts/darts-train.sh
+++ scripts/darts-train.sh
@@ -8,7 +8,7 @@ epochs=600
 base_save_dir="results/darts_train"
 
 # Define the architecture and seeds
-architectures=("BETASAM_exp1")
+architectures=("SAM_exp_bad")
 seeds=(1)
 
 # Loop through each architecture
